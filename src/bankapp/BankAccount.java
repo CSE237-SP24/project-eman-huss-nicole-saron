819e212d4340340fe5f1f252c7e1fdf6acf36e7f
huss2342 <89368091+huss2342@users.noreply.github.com>
2024-03-19 15:50:31 -0500

    almost heaven

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -2,22 +2,26 @@ package bankapp;
 
 // a hash set doesnt allow duplicates and doesnt maintain insertion order
 import java.util.HashSet;
+import java.util.Set;
 
 public class BankAccount {
 	
-	private double balance;
+	private double balance; // we can add balance to be part of the constructor
 	private String accountName = "";
-	private HashSet<String> allAccountNames = new HashSet<String>();
+	private static Set<String> allAccountNames = new HashSet<>();
 	
+	//-- Constructors are private because they shouldn't be called on from the outside, call the createAccount method instead
 	// Constructor with an account name
-	public BankAccount(String name) {
+	private BankAccount(String name) {
 		this.accountName = name;
 		this.balance = 0;
-	}
-	
-	// Constructor without an account name
-	public BankAccount() {
-		this.balance = 0;
+		/*
+		 this.depositHandler = new DepositHandler();
+		 this.withdrawHandler = new WithdrawHandler();
+		 this.transferHandler = new TransferHandler();
+		 // we can do all the above or we can make a generic transaction handler class 
+		 // that then deals with making these handlers
+		*/
 	}
 
 	public String getAccountName() {
@@ -29,24 +33,40 @@ public class BankAccount {
 	}
 	
 	// Factory method for the constructor with the name
-	public BankAccount createAccount(String name) {
-		// handles duplicate name
-		if (!allAccountNames.add(name)) {
-			System.err.println("This account name is already taken");
-			return null;
+	//-- This and the other createAccount method would be the one to use when making an account
+	public static BankAccount createAccount(String name) {
+		if (name == null || name.isEmpty()) {
+			System.err.print("FATAL ERROR: name cannot be null !!");
 		}
-		return new BankAccount(name);
+	    
+		if (!allAccountNames.contains(name)) {
+	        System.err.println("This account name is already taken");
+	        return null;
+	    }
+	    
+		allAccountNames.add(name);
+	    return new BankAccount(name);
 	}
+	
+	// TODO: add a createAccount method that also takes in an initial amount of cash ?
 	
 	// Factory method for the constructor without a name
-	public BankAccount createAccount() {
-		return new BankAccount();
+	public static BankAccount createAccount() {
+		// these temporary nameless accounts wont be added to the allAccountNames set
+	    return new BankAccount(null);
 	}
 	
-	public HashSet<String> getAllAccountNames() {
+	public Set<String> getAllAccountNames() {
 		return allAccountNames;
 	}
 	
+	/*// - my vision on how to use the handler method would be like
+	 public void deposit(double amount){
+	 	depositHandler.depositIntoAccount(this, amount)
+	 	// the deposit method would look like: public void depositIntoAccount(BankAccount account, double amount)
+	 }
+	 
+	 */
 	
 	//public method doing some work - lots of tests
 	public void deposit(double amount) {
