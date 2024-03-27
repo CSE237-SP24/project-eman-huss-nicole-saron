f2ea352f4ad5e0b3b74a635b4a8cb5ef73b22d79
Eman Teshome <eman@wustl.edu>
2024-03-26 22:49:49 -0500

    include withdraw handler

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -76,5 +76,9 @@ public class BankAccount {
     public static BankAccount getAccountByName(String name) {
         return allAccounts.get(name);
     }
+    
+    public void withdraw(double amount){
+	 	withdrawHandler.withdrawInAccount(this, amount);
+	}
 }
 
