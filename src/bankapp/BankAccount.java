20e609efc9d3b5b3660ef67a8c62b8bfbdff9099
humanwell <nicole.dai@humanwellus.com>
2024-04-01 11:16:28 -0500

    fixed handlers

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -8,9 +8,9 @@ public class BankAccount {
 
 	private double balance; // we can add balance to be part of the constructor
 	private String accountName = "";
-	private DepositHandler depositHandler;
-	private WithdrawHandler withdrawHandler;
-	private TransferHandler transferHandler;
+//	private DepositHandler depositHandler;
+	//private WithdrawHandler withdrawHandler;
+	//private TransferHandler transferHandler;
     private static Map<String, BankAccount> allAccounts = new HashMap<>();
     
 	// -- Constructors are private because they shouldn't be called on from the
@@ -20,9 +20,9 @@ public class BankAccount {
 		this.accountName = name;
 		this.balance = 0;
 
-		this.depositHandler = new DepositHandler();
-    	this.withdrawHandler = new WithdrawHandler();
-		this.transferHandler = new TransferHandler();
+//		this.depositHandler = new DepositHandler();
+//    	this.withdrawHandler = new WithdrawHandler();
+		//this.transferHandler = new TransferHandler();
 	}
 
 	public String getAccountName() {
@@ -37,6 +37,7 @@ public class BankAccount {
 		this.balance += amount;
 	}
 	public void removeBalance(double amount) {
+		
 		this.balance -= amount;
 	}
 
@@ -64,10 +65,58 @@ public class BankAccount {
 	}
 
 	public void deposit(double amount){
-	 	depositHandler.depositInAccount(this, amount);
+		if (!transactionErrorHandler(this, amount)) {
+			return;
+		}
+		this.addBalance(amount);
 	}
-	public void transfer(BankAccount receivingAccount, double amount){
-	 	transferHandler.transfer(this, receivingAccount,amount);
+	private boolean transactionErrorHandler(BankAccount account, double amount) {
+		if (amount < 0) {
+			System.err.println("Amount must be positive");
+			return false;
+		} else if (account.getBalance() + amount < account.getBalance()) {
+			System.err.println("Account is too full");
+			return false;
+		} else if (amount == Double.POSITIVE_INFINITY) {
+			System.err.println("Deposit is too large. Add a smaller deposit.");
+			return false;
+		} else if (account.getBalance() + amount == Double.POSITIVE_INFINITY) {
+			System.err.println("Account overflowed. Add a smaller deposit.");
+			return false;
+		}
+		// unclear on how to test this, but just to be safe
+		else if (account.getBalance() + amount == Double.NEGATIVE_INFINITY
+				|| account.getBalance() == Double.NEGATIVE_INFINITY) {
+			System.err.println("Amount must be positive.");
+			return false;
+		}
+		return true;
+
+	}
+	public void transfer(BankAccount receivingAccount, double amount) {
+		if(amount < 0) {
+			throw new IllegalArgumentException("Amount must be positive");
+		} 
+		if(amount >  receivingAccount.getBalance()) {
+			throw new IllegalArgumentException("You are overdrafting your account. Your balance is: " + this.getBalance() + ". Please transfer an amount less than or equal to your balance.");
+		}
+		/*
+		 * if (!this.removeBalance(amount)
+		 * 	return amount is invalid
+		 * 
+		 * if(receivingAccount ==null)		
+		 * 	return receivingAccount Not found	
+		 * 
+		 * if (!receivingAccount.addBalance(amount)
+		 * 	return amount is invalid
+		 *  
+		 * return success
+		 * 
+		 */
+		
+		// changethe rempve and add
+		this.removeBalance(amount);
+		receivingAccount.addBalance(amount);
 	}
 	
     public static BankAccount getAccountByName(String name) {
@@ -75,7 +124,20 @@ public class BankAccount {
     }
     
     public void withdraw(double amount){
-	 	withdrawHandler.withdrawInAccount(this, amount);
+    	if (!withdrawErrorHandler(this, amount)) {
+			return;
+		}
+		this.removeBalance(amount);
 	}
+    private boolean withdrawErrorHandler(BankAccount account, double amount) {
+		if (amount <= 0 || amount == Double.NEGATIVE_INFINITY) {
+			System.err.println("Amount too small, must be positive");
+			return false;
+		} else if ((account.getBalance() - amount < 0) || amount == Double.POSITIVE_INFINITY) {
+			System.err.println("Withdraw account exceeds balance");
+			return false;
+		} 
+		return true;
+    }
 }
 
